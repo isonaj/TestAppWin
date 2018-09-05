5c369722f6831755edca3b33c74ef4fd02ce5fb4
Anthony <isonaj@gmail.com>
2018-09-05 22:04:34 +1000

    Initial changes. Basic app

--- TestAppWin/Pages/Index.cshtml.cs
+++ TestAppWin/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,9 +10,12 @@ namespace TestAppWin.Pages
 {
     public class IndexModel : PageModel
     {
+        public IEnumerable<string> MyFiles { get; set; }
         public void OnGet()
         {
-
+            if (!Directory.Exists("data"))
+                Directory.CreateDirectory("data");
+            MyFiles = Directory.EnumerateFiles("data");
         }
     }
 }
